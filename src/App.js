8df0ffec084a326143b04e08ad108b3bed6382e4
vaibhav-kundaliya <vaibhav.kundaliya@iviewlabs.net>
2024-04-03 18:59:13 +0530

    components are selected from ant design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from "@ant-design/icons";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Layout, Menu, theme } from "antd";
+import StepperForm from "./components/stepperForm"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Header, Content, Footer, Sider } = Layout;
 
+const items = [
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    UserOutlined,
+].map((icon, index) => ({
+    key: String(index + 1),
+    icon: React.createElement(icon),
+    label: `nav ${index + 1}`,
+}));
+
+const App = () => {
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Router>
+            <Layout>
+                <Sider
+                    breakpoint="lg"
+                    collapsedWidth="0"
+                    onBreakpoint={(broken) => {
+                        console.log(broken);
+                    }}
+                    onCollapse={(collapsed, type) => {
+                        console.log(collapsed, type);
+                    }}
+                >
+                    <div className="demo-logo-vertical" />
+                    <Menu
+                        theme="dark"
+                        mode="inline"
+                        defaultSelectedKeys={["1"]}
+                    >
+                        <Menu.Item key="1">
+                            <Link to="/">
+                                <span>Submit Profile</span>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link to="/profile-status">
+                                <span>Profile Status</span>
+                            </Link>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Content
+                        style={{
+                            margin: "24px 16px 0",
+                        }}
+                    >
+                        <div
+                            style={{
+                                padding: 24,
+                                minHeight: "90vh",
+                                background: colorBgContainer,
+                                borderRadius: borderRadiusLG,
+                            }}
+                        >
+                          <Routes>
+                            <Route path="/" element={<StepperForm />}/>
+                            <Route path="/profile-status" />
+                          </Routes>
+                        </div>
+                    </Content>
+                    <Footer
+                        style={{
+                            textAlign: "center",
+                        }}
+                    >
+                        Ant Design ©{new Date().getFullYear()} Created by Ant
+                        UED
+                    </Footer>
+                </Layout>
+            </Layout>
+        </Router>
+    );
+};
 export default App;
